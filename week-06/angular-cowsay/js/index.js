fbabc4e52d2e7dcd22df041ea4e0328c25d3fbb6
Duncan Marsh <dwolfm@gmail.com>
2016-06-13 00:14:53 -0700

    inital cowsay attempt

---
+++ week-06/angular-cowsay/js/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const angular = require('angular')
+const cowsay = require('cowsay-browser')
+const lorem = require('lorem-ipsum')
+
+const noteApp = angular.module('noteapp', [])
+noteApp.controller('CowsayController', function(){
+  this.animals = []
+  this.cowsayFile = 'default'
+  this.cowsayText = 'Wish i had something better to say !'
+
+  this.getAnimals = () => {
+    cowsay.list((err, list) => {
+      this.animals = list
+    })
+  }
+
+  this.loremify = () => {
+    this.cowsayText = lorem({count: 4, units: 'words'})
+  }
+
+  this.say = (text) => {
+    return '\n' + cowsay.say({text: text , f: this.cowsayFile}) 
+  }
+})
