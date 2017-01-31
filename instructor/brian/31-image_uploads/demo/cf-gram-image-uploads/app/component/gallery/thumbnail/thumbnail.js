f83aa162871017c39ed2e58428734d2945f43e4d
bnates <brian.nations@codefellows.com>
2017-01-30 17:13:45 -0800

    added lecture code and wireframes

---
+++ instructor/brian/31-image_uploads/demo/cf-gram-image-uploads/app/component/gallery/thumbnail/thumbnail.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  template: require('./thumbnail.html'),
+  controller: ['$log', 'picService', ThumbnailController],
+  controllerAs: 'thumbnailCtrl',
+  bindings: {
+    pic: '<'
+  }
+};
+
+function ThumbnailController($log, picService) {
+  $log.debug('ThumbnailController');
+
+  this.deletePic = function() {
+    $log.debug('thumbnailCtrl.deletePic');
+  };
+};
---
+++ instructor/brian/31-image_uploads/demo/cf-gram-image-uploads/app/component/gallery/upload-pic/upload-pic.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  template: require('./upload-pic.html'),
+  controller: ['$log', 'picService', UploadPicController],
+  controllerAs: 'uploadPicCtrl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function UploadPicController($log, picService) {
+  $log.debug('UploadPicController');
+
+  this.pic = {};
+
+  this.uploadPic = function() {
+    picService.uploadGalleryPic(this.gallery, this.pic)
+    .then( () => {
+      this.pic.name = null;
+      this.pic.desc = null;
+      this.pic.file = null;
+    })
+  }
+}
