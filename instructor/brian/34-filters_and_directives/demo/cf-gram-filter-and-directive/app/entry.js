92f9d51b090e7065efed37679f6fe32df6bd2d86
bnates <brian.nations@codefellows.com>
2017-02-07 10:12:17 -0800

    added demo

--- instructor/brian/34-filters_and_directives/demo/cf-gram-filter-and-directive/app/entry.js
+++ instructor/brian/34-filters_and_directives/demo/cf-gram-filter-and-directive/app/entry.js
@@ -38,3 +38,17 @@ context.keys().forEach( key => {
   let module = context(key);
   cfgram.component(name, module);
 });
+
+context = require.context('./directive/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.directive(name, module);
+});
+
+context = require.context('./filter/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.filter(name, module);
+});
