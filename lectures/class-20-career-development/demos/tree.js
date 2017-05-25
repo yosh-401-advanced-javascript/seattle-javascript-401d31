8642de83d1fcd4268dd8a3d254564a660b5f30ab
Scott Schmidt <sjschmidt44@gmail.com>
2017-05-25 11:03:58 -0700

    add tree demo

---
+++ lectures/class-20-career-development/demos/tree.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Node = function(val) {
+  this.val = val
+  this.children = []
+}
+
+const Tree = module.exports = function() {
+  this.root = null
+}
+
+Tree.prototype.add = function(val, parentVal) {
+  let newNode = new Node(val)
+
+  if(!this.root) {
+    this.root = newNode
+    return
+  }
+
+  this.preOrder(node => {
+    if(node.val === parentVal) {
+      node.children.push(newNode)
+      return
+    }
+  })
+}
+
+Tree.prototype.preOrder = function(cb) {
+  _walk(this.root)
+
+  function _walk(node) {
+    console.log(node.val);
+    cb(node)
+    node.children.forEach(_walk)
+  }
+}
