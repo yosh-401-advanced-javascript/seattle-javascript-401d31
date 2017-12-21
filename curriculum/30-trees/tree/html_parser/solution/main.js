e27225f1b4a6c11f0c8ab3122fd4ca5e1417e170
Steve Geluso <stevegeluso@gmail.com>
2017-12-21 14:23:28 -0800

    adding tiny.html example. removing json type from readme.

--- curriculum/30-trees/tree/html_parser/solution/main.js
+++ curriculum/30-trees/tree/html_parser/solution/main.js
@@ -1,7 +1,8 @@
 const parser = require('./parser');
 
-let filePath = '../minimal.html';
+// there's tiny.html, minimal.html and stretch.html
+let filePath = '../tiny.html';
 if (process.argv.length > 2) {
-  filePath = argv[2];
+  filePath = process.argv[2];
 }
 parser(filePath);
