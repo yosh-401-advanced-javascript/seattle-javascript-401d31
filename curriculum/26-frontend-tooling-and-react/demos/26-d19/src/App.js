1fabdfe6f4330c4d0e0c4d74baabb6ebab6a5d59
Steve Geluso <stevegeluso@gmail.com>
2018-01-05 15:54:09 -0800

    moving lab26 d19 solution

---
+++ curriculum/26-frontend-tooling-and-react/demos/26-d19/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import faker from 'faker';
+import cowsay from 'cowsay-browser';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {content: 'heeeelllooo', cows: []};
+
+    this.generateText = this.generateText.bind(this);
+    this.setCow = this.setCow.bind(this);
+  }
+
+  componentWillMount() {
+    cowsay.list((err, cows) => {
+      let current = cows[0]
+      this.setState({cows, current})
+    });
+  }
+
+  generateText() {
+    let cow = this.state.current || 'moofasa';
+    let cowSaid = cowsay.say({text: faker.random.words(4), f: cow});
+    this.setState({content: cowSaid})
+  }
+
+  setCow(e) {
+    this.setState({current: e.target.value})
+  }
+
+  cows() {
+    return this.state.cows.map((cow, index) => {
+      return <option value={cow} key={index}>{cow}</option>
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Generate Cowsay Lorem</h1>
+        </header>
+        <p className="App-intro">
+          <select onChange={this.setCow}>
+            <option>choose a cow</option>
+            {this.cows()}
+          </select>
+          <button onClick={this.generateText}>then click me!</button>
+        </p>
+        <pre>{this.state.content}</pre>
+      </div>
+    );
+  }
+}
+
+export default App;
