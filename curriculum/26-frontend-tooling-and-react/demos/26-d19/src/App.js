b37a4168c088146c6ec1ae9be6c12391568069f6
Steve Geluso <stevegeluso@gmail.com>
2018-01-05 15:56:26 -0800

    cleaning up unused stuff

--- curriculum/26-frontend-tooling-and-react/demos/26-d19/src/App.js
+++ curriculum/26-frontend-tooling-and-react/demos/26-d19/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import faker from 'faker';
 import cowsay from 'cowsay-browser';
@@ -40,7 +39,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Generate Cowsay Lorem</h1>
         </header>
         <p className="App-intro">
