ea5e1cb17806bccc81442f19d6515b8aacebda43
Steve Geluso <stevegeluso@gmail.com>
2018-01-18 12:11:28 -0800

    responding to code review

--- curriculum/27-forms-and-props/demos/27-d19/demo/forms/src/components/playground-form.js
+++ curriculum/27-forms-and-props/demos/27-d19/demo/forms/src/components/playground-form.js
@@ -2,23 +2,18 @@ import React from 'react';
 
 class PlayGroundForm extends React.Component {
   constructor(props) {
-    super(props)
-
     this.state = {
       name: '',
       description: ''
     }
 
-    this.handleNameChange = this.handleNameChange.bind(this)
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  handleNameChange(event) {
-    this.setState({name: event.target.value})
-  }
-
-  handleDescriptionChange(event) {
-    this.setState({description: event.target.value})
+  handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({name: value})
   }
 
   render() {
@@ -28,12 +23,9 @@ class PlayGroundForm extends React.Component {
           <p>
             Name:
             <input size="40" type="text" value={this.state.name}
-              onChange={this.handleNameChange}
               placeholder="Jefferson G. Memorial Park"/>
           </p>
           <textarea value={this.state.description}
-            onChange={this.handleDescriptionChange}
-            cols="60" rows="20"
             placeholder="This park has many nice features...">
           </textarea>
         </div>
--- curriculum/27-forms-and-props/demos/27-d19/demo/forms/src/index.js
+++ curriculum/27-forms-and-props/demos/27-d19/demo/forms/src/index.js
@@ -1,51 +1,12 @@
 import './style/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PlayGroundForm from './components/playground-form'
-import ResultList from './components/result-list'
-import SearchForm from './components/search-form'
+import FormContainer from './components/form-container'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      results: [],
-      hasSearched: false,
-      loading: false
-    }
-
-    this.search = this.search.bind(this)
-    this.setResults = this.setResults.bind(this)
-  }
-
-  search(filter='') {
-    this.setState({loading: true, hasSearched: true})
-    setTimeout(() => {
-      let newResults = [
-          {language: 'python', rating: 8},
-          {language: 'java', rating: 6},
-          {language: 'javascript', rating: 9},
-          {language: 'php', rating: 2},
-      ].filter(result => {
-        return result.language.includes(filter)
-      })
-      this.setResults(newResults)
-    }, 1000)
-  }
-
-  setResults(newResults) {
-    this.setState({results: newResults, loading: false})
-  }
-
   render() {
     return (
-      <div>
-        <SearchForm submitSearch={this.search} />
-        <ResultList
-          results={this.state.results}
-          hasSearched={this.state.hasSearched}
-          loading={this.state.loading} />
-      </div>
+      <FormContainer/>
     )
   }
 }
