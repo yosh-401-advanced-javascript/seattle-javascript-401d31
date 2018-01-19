7192a7ba9539d6b2e28cf742b6ff2976772a0f30
Steve Geluso <stevegeluso@gmail.com>
2018-01-19 15:28:55 -0800

    replacing it() calls with test() for jest

--- curriculum/10-linked-lists/__test__/singly-linked-list.test.js
+++ curriculum/10-linked-lists/__test__/singly-linked-list.test.js
@@ -4,7 +4,7 @@ const expect = require('expect');
 const SLL = require('../singly-linked-list');
 
 describe('testing singlyLinkedList', function(){
-  it('new SLL should create a node', () => {
+  test('new SLL should create a node', () => {
     let node = new SLL(4);
     expect(node.value).toEqual(4);
     expect(node.next).toEqual(null);
@@ -14,7 +14,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next).toEqual(node);
   });
 
-  it('SLL.fromArray should return a linked list', () => {
+  test('SLL.fromArray should return a linked list', () => {
     let head = SLL.fromArray([1,2,3,4])
     expect(head.value).toEqual(1)
     expect(head.next.value).toEqual(2)
@@ -22,7 +22,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next.next.next.value).toEqual(4)
   })
 
-  it('SLL.append(list, node, should append node to list', () => {
+  test('SLL.append(list, node, should append node to list', () => {
     let head = new SLL(0);
     SLL.append(head, new SLL(3));
     SLL.append(head, new SLL(4));
@@ -31,7 +31,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next.next.value).toEqual(4);
   });
 
-  it('node.append(value), should append node to list', () => {
+  test('node.append(value), should append node to list', () => {
     let head = new SLL(0)
     head.append(new SLL(4)).append(new SLL(5))
     expect(head.value).toEqual(0)
@@ -39,7 +39,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next.next.value).toEqual(5)
   });
 
-  it('SLL.prepend(list, node) should return a list with node as head', () => {
+  test('SLL.prepend(list, node) should return a list with node as head', () => {
     let list = SLL.fromArray([0, 4, 5])
     let head = new SLL(-1);
     let result = SLL.prepend(list, head);
@@ -48,7 +48,7 @@ describe('testing singlyLinkedList', function(){
     expect(result).toEqual(head);
   })
 
-  it('list.prepend(node) should return a list with node as head', () => {
+  test('list.prepend(node) should return a list with node as head', () => {
     let list = SLL.fromArray([0, 4, 5])
     let head = new SLL(-1);
     let result = list.prepend(head);
@@ -57,7 +57,7 @@ describe('testing singlyLinkedList', function(){
     expect(result).toEqual(head);
   })
 
-  it('SLL.remove(list, node) should remove a list with node as head', () => {
+  test('SLL.remove(list, node) should remove a list with node as head', () => {
     let removeMe = new SLL('delete')
     let head = new SLL('head')
 
@@ -71,7 +71,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next.value).toEqual('first')
   });
 
-  it('list.remove(node) should remove a list with node as head', () => {
+  test('list.remove(node) should remove a list with node as head', () => {
     let removeMe = new SLL('delete');
     let head = new SLL('head');
 
@@ -84,7 +84,7 @@ describe('testing singlyLinkedList', function(){
     expect(head.next.value).toEqual('first');
   });
 
-  it('SLL.reverse(list) should reverse the list', () => {
+  test('SLL.reverse(list) should reverse the list', () => {
     let list = SLL.fromArray([4, 5, 6]);
     let result = SLL.reverse(list);
     expect(result.value).toEqual(6);
@@ -92,7 +92,7 @@ describe('testing singlyLinkedList', function(){
     expect(result.next.next.value).toEqual(4);
   });
 
-  it('list.reverse() should reverse the list', () => {
+  test('list.reverse() should reverse the list', () => {
     let list = SLL.fromArray([4, 5, 6]);
     let result = list.reverse();
     expect(result.value).toEqual(6);
@@ -100,7 +100,7 @@ describe('testing singlyLinkedList', function(){
     expect(result.next.next.value).toEqual(4);
   });
 
-  it('SLL.getMiddle(list) should return middle node', () => {
+  test('SLL.getMiddle(list) should return middle node', () => {
     let list = SLL.fromArray([4, 5, 6]);
     let middle = SLL.getMiddle(list);
     expect(middle.value).toEqual(5);
@@ -110,7 +110,7 @@ describe('testing singlyLinkedList', function(){
     expect(middle.value).toEqual(5);
   });
 
-  it('list.getMiddle() should return middle node', () => {
+  test('list.getMiddle() should return middle node', () => {
     let list = SLL.fromArray([4, 5, 6]);
     let middle = list.getMiddle();
     expect(middle.value).toEqual(5);
@@ -120,43 +120,43 @@ describe('testing singlyLinkedList', function(){
     expect(middle.value).toEqual(5);
   });
 
-  it('list.getLast() should return last node', () => {
+  test('list.getLast() should return last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = list.getLast();
     expect(result.value).toEqual(6);
   });
 
-  it('SLL.getLast(list) should return last  node', () => {
+  test('SLL.getLast(list) should return last  node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = SLL.getLast(list);
     expect(result.value).toEqual(6);
   });
 
-  it('SLL.getSecondFromLast(list) should return second to last node', () => {
+  test('SLL.getSecondFromLast(list) should return second to last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = SLL.getSecondFromLast(list);
     expect(result.value).toEqual(5);
   });
 
-  it('list.getSecondFromLast() should return second to last node', () => {
+  test('list.getSecondFromLast() should return second to last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = list.getSecondFromLast();
     expect(result.value).toEqual(5);
   });
 
-  it('SLL.getThirdFromLast(list) should return third from lastnode', () => {
+  test('SLL.getThirdFromLast(list) should return third from lastnode', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = SLL.getThirdFromLast(list);
     expect(result.value).toEqual(4);
   });
 
-  it('list.getThirdFromLast() should return third from last node', () => {
+  test('list.getThirdFromLast() should return third from last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = list.getThirdFromLast();
     expect(result.value).toEqual(4);
   });
 
-  it('list.getNthFromLast() should return nth from last node', () => {
+  test('list.getNthFromLast() should return nth from last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = list.getNthFromLast(0);
     expect(result.value).toEqual(6);
@@ -171,7 +171,7 @@ describe('testing singlyLinkedList', function(){
     expect(result.value).toEqual(3);
   });
 
-  it('SLL.getNthFromLast(list, n) should return nth from last node', () => {
+  test('SLL.getNthFromLast(list, n) should return nth from last node', () => {
     let list = SLL.fromArray([3, 4, 5, 6]);
     let result = SLL.getNthFromLast(list, 0)
     expect(result.value).toEqual(6);
@@ -186,25 +186,25 @@ describe('testing singlyLinkedList', function(){
     expect(result.value).toEqual(3);
   });
 
-  it('list.reduce(cb) should reduce list', () => {
+  test('list.reduce(cb) should reduce list', () => {
     let list = SLL.fromArray([1,2,3]);
     let result = list.reduce((p, n) => p + n.value, 0);
     expect(result).toEqual(6);
   });
 
-  it('list.map(doubler) should double a list', () => {
+  test('list.map(doubler) should double a list', () => {
     let list = SLL.fromArray([1,2,3]);
     let result = list.map(n => n.value * 2);
     expect(result).toEqual(SLL.fromArray([2,4,6]));
   });
 
-  it('list.filter(isEven) should filter out odd numbers', () => {
+  test('list.filter(isEven) should filter out odd numbers', () => {
     let list = SLL.fromArray([1,2,3,4]);
     let result = list.filter(n => n.value % 2 === 0);
     expect(result).toEqual(SLL.fromArray([2,4]));
   })
 
-  it('list.find(value) return fist node containg value', () => {
+  test('list.find(value) return fist node containg value', () => {
     let list = SLL.fromArray([1,2,3,2,1]);
     let result = list.find(2);
     expect(list.next).toEqual(result);
@@ -212,7 +212,7 @@ describe('testing singlyLinkedList', function(){
     expect(result).toEqual(list.getNthFromLast(1));
   });
 
-  it('getNth shuold return the corect node', () => {
+  test('getNth shuold return the corect node', () => {
     let list = SLL.fromArray([1,2,3,4]);
     expect(list.getNth(0)).toEqual(list);
     expect(list.getNth(1)).toEqual(list.next);
@@ -220,7 +220,7 @@ describe('testing singlyLinkedList', function(){
     expect(list.getNth(3)).toEqual(list.next.next.next);
   });
 
-  it('getFirst getSecond and getThird', () => {
+  test('getFirst getSecond and getThird', () => {
     let list = SLL.fromArray([1,2,3,4]);
     expect(list.getFirst()).toEqual(list);
     expect(list.getSecond()).toEqual(list.next);
