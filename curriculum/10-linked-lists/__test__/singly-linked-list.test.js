1d6eff346ec269113aa653775d1284b9c0380293
Steve Geluso <stevegeluso@gmail.com>
2018-01-19 16:57:44 -0800

    rewriting LinkedList and tests. simplyfying. improving readability.

--- curriculum/10-linked-lists/__test__/singly-linked-list.test.js
+++ curriculum/10-linked-lists/__test__/singly-linked-list.test.js
@@ -1,230 +1,221 @@
 'use strict'
 
-const SLL = require('../singly-linked-list');
+const {LinkedList, ListNode} = require('../singly-linked-list');
 
-describe('testing singlyLinkedList', function(){
-  test('new SLL should create a node', () => {
-    let node = new SLL(4);
-    expect(node.value).toEqual(4);
-    expect(node.next).toEqual(null);
+describe('testing LinkedList', function(){
+  test('new LinkedList should create a node', () => {
+    let blue = new ListNode(4);
+    expect(blue.value).toEqual(4);
+    expect(blue.next).toEqual(null);
 
-    let head = new SLL(3, node);
-    expect(head.value).toEqual(3);
-    expect(head.next).toEqual(node);
+    let green = new ListNode(3, blue);
+    expect(green.value).toEqual(3);
+    expect(green.next).toEqual(blue);
+    expect(green.next.value).toEqual(4);
   });
 
-  test('SLL.fromArray should return a linked list', () => {
-    let head = SLL.fromArray([1,2,3,4])
-    expect(head.value).toEqual(1)
-    expect(head.next.value).toEqual(2)
-    expect(head.next.next.value).toEqual(3)
-    expect(head.next.next.next.value).toEqual(4)
+  test('list.fromArray should return a linked list', () => {
+    let list = new LinkedList();
+    list.fromArray([1,2,3,4])
+    expect(list.root.value).toEqual(1)
+    expect(list.root.next.value).toEqual(2)
+    expect(list.root.next.next.value).toEqual(3)
+    expect(list.root.next.next.next.value).toEqual(4)
   })
 
-  test('SLL.append(list, node, should append node to list', () => {
-    let head = new SLL(0);
-    SLL.append(head, new SLL(3));
-    SLL.append(head, new SLL(4));
-    expect(head.value).toEqual(0);
-    expect(head.next.value).toEqual(3);
-    expect(head.next.next.value).toEqual(4);
+  test('append(value) should append node to empty list', () => {
+    let list = new LinkedList();
+    list.append(0);
+    expect(list.root.value).toBe(0);
   });
 
-  test('node.append(value), should append node to list', () => {
-    let head = new SLL(0)
-    head.append(new SLL(4)).append(new SLL(5))
-    expect(head.value).toEqual(0)
-    expect(head.next.value).toEqual(4)
-    expect(head.next.next.value).toEqual(5)
+
+  test('append(value) should append node to non-empty list', () => {
+    let list = new LinkedList();
+    list.append(0);
+    list.append(3);
+    list.append(4);
+    expect(list.root.value).toEqual(0);
+    expect(list.root.next.value).toEqual(3);
+    expect(list.root.next.next.value).toEqual(4);
   });
 
-  test('SLL.prepend(list, node) should return a list with node as head', () => {
-    let list = SLL.fromArray([0, 4, 5])
-    let head = new SLL(-1);
-    let result = SLL.prepend(list, head);
-    expect(result.value).toEqual(-1);
-    expect(result.next).toEqual(list);
-    expect(result).toEqual(head);
-  })
-
-  test('list.prepend(node) should return a list with node as head', () => {
-    let list = SLL.fromArray([0, 4, 5])
-    let head = new SLL(-1);
-    let result = list.prepend(head);
-    expect(result.value).toEqual(-1);
-    expect(result.next).toEqual(list);
-    expect(result).toEqual(head);
-  })
-
-  test('SLL.remove(list, node) should remove a list with node as head', () => {
-    let removeMe = new SLL('delete')
-    let head = new SLL('head')
-
-    SLL.append(head, removeMe)
-    SLL.append(head, new SLL('first'))
-    expect(head.value).toEqual('head')
-    expect(head.next).toEqual(removeMe)
-
-    SLL.remove(head, removeMe)
-    expect(head.value).toEqual('head')
-    expect(head.next.value).toEqual('first')
+  test('prepend(node) should node to empty list', () => {
+    let list = new LinkedList();
+    list.prepend(5)
+    expect(list.root.value).toBe(5);
   });
 
-  test('list.remove(node) should remove a list with node as head', () => {
-    let removeMe = new SLL('delete');
-    let head = new SLL('head');
-
-    head.append(removeMe).append(new SLL('first'));
-    expect(head.value).toEqual('head');
-    expect(head.next).toEqual(removeMe);
-
-    head.remove(removeMe);
-    expect(head.value).toEqual('head');
-    expect(head.next.value).toEqual('first');
+  test('prepend(node) should node to non-empty list', () => {
+    let list = new LinkedList();
+    list.prepend(5)
+    list.prepend(4)
+    list.prepend(0)
+    expect(list.root.value).toBe(0);
+    expect(list.root.next.value).toBe(4);
+    expect(list.root.next.next.value).toBe(5);
   });
 
-  test('SLL.reverse(list) should reverse the list', () => {
-    let list = SLL.fromArray([4, 5, 6]);
-    let result = SLL.reverse(list);
-    expect(result.value).toEqual(6);
-    expect(result.next.value).toEqual(5);
-    expect(result.next.next.value).toEqual(4);
+  test('remove(value) should remove node from one-element list', () => {
+    let list = new LinkedList()
+    list.append(42);
+    list.remove(42);
+    expect(list.root).toBe(null)
   });
 
-  test('list.reverse() should reverse the list', () => {
-    let list = SLL.fromArray([4, 5, 6]);
-    let result = list.reverse();
-    expect(result.value).toEqual(6);
-    expect(result.next.value).toEqual(5);
-    expect(result.next.next.value).toEqual(4);
+  test('remove(value) should remove node from front of list', () => {
+    let list = new LinkedList();
+    list.fromArray([0,1,2,3,4,5,6,7,8])
+    list.remove(0);
+    expect(list.root.value).toBe(1);
   });
 
-  test('SLL.getMiddle(list) should return middle node', () => {
-    let list = SLL.fromArray([4, 5, 6]);
-    let middle = SLL.getMiddle(list);
-    expect(middle.value).toEqual(5);
-
-    list = SLL.fromArray([3, 4, 5, 6]);
-    middle = SLL.getMiddle(list);
-    expect(middle.value).toEqual(5);
+  test('remove(value) should remove node from middle of list', () => {
+    let list = new LinkedList();
+    list.fromArray([0,1,2,3,4,5,6,7,8])
+    list.remove(3);
+    expect(list.root.next.next.value).toBe(2);
+    expect(list.root.next.next.next.value).toBe(4);
   });
 
-  test('list.getMiddle() should return middle node', () => {
-    let list = SLL.fromArray([4, 5, 6]);
+  test('remove(value) should remove node from end of list', () => {
+    let list = new LinkedList();
+    list.fromArray([0,1,2,3,4,5,6,7,8])
+    list.remove(8);
+    expect(list.root.next.next.next.next.next.next.next.value).toBe(7);
+    expect(list.root.next.next.next.next.next.next.next.next).toBe(null);
+  });
+
+  // test('SLL.reverse(list) should reverse the list', () => {
+  //   let list = SLL.fromArray([4, 5, 6]);
+  //   let result = SLL.reverse(list);
+  //   expect(result.value).toEqual(6);
+  //   expect(result.next.value).toEqual(5);
+  //   expect(result.next.next.value).toEqual(4);
+  // });
+
+  // test('list.reverse() should reverse the list', () => {
+  //   let list = SLL.fromArray([4, 5, 6]);
+  //   let result = list.reverse();
+  //   expect(result.value).toEqual(6);
+  //   expect(result.next.value).toEqual(5);
+  //   expect(result.next.next.value).toEqual(4);
+  // });
+
+  test('getMiddle() should return middle node', () => {
+    let list = (new LinkedList()).fromArray([4, 5, 6]);
     let middle = list.getMiddle();
     expect(middle.value).toEqual(5);
 
-    list = SLL.fromArray([3, 4, 5, 6]);
+    list = (new LinkedList()).fromArray([3, 4, 5, 6]);
     middle = list.getMiddle();
     expect(middle.value).toEqual(5);
   });
 
-  test('list.getLast() should return last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = list.getLast();
-    expect(result.value).toEqual(6);
-  });
+  // test('list.getLast() should return last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = list.getLast();
+  //   expect(result.value).toEqual(6);
+  // });
 
-  test('SLL.getLast(list) should return last  node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = SLL.getLast(list);
-    expect(result.value).toEqual(6);
-  });
+  // test('SLL.getLast(list) should return last  node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = SLL.getLast(list);
+  //   expect(result.value).toEqual(6);
+  // });
 
-  test('SLL.getSecondFromLast(list) should return second to last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = SLL.getSecondFromLast(list);
-    expect(result.value).toEqual(5);
-  });
+  // test('SLL.getSecondFromLast(list) should return second to last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = SLL.getSecondFromLast(list);
+  //   expect(result.value).toEqual(5);
+  // });
 
-  test('list.getSecondFromLast() should return second to last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = list.getSecondFromLast();
-    expect(result.value).toEqual(5);
-  });
+  // test('list.getSecondFromLast() should return second to last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = list.getSecondFromLast();
+  //   expect(result.value).toEqual(5);
+  // });
 
-  test('SLL.getThirdFromLast(list) should return third from lastnode', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = SLL.getThirdFromLast(list);
-    expect(result.value).toEqual(4);
-  });
+  // test('SLL.getThirdFromLast(list) should return third from lastnode', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = SLL.getThirdFromLast(list);
+  //   expect(result.value).toEqual(4);
+  // });
 
-  test('list.getThirdFromLast() should return third from last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = list.getThirdFromLast();
-    expect(result.value).toEqual(4);
-  });
+  // test('list.getThirdFromLast() should return third from last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = list.getThirdFromLast();
+  //   expect(result.value).toEqual(4);
+  // });
 
-  test('list.getNthFromLast() should return nth from last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = list.getNthFromLast(0);
-    expect(result.value).toEqual(6);
+  // test('list.getNthFromLast() should return nth from last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = list.getNthFromLast(0);
+  //   expect(result.value).toEqual(6);
 
-    result = list.getNthFromLast(1);
-    expect(result.value).toEqual(5);
+  //   result = list.getNthFromLast(1);
+  //   expect(result.value).toEqual(5);
 
-    result = list.getNthFromLast(2)
-    expect(result.value).toEqual(4);
+  //   result = list.getNthFromLast(2)
+  //   expect(result.value).toEqual(4);
 
-    result = list.getNthFromLast(3)
-    expect(result.value).toEqual(3);
-  });
+  //   result = list.getNthFromLast(3)
+  //   expect(result.value).toEqual(3);
+  // });
 
-  test('SLL.getNthFromLast(list, n) should return nth from last node', () => {
-    let list = SLL.fromArray([3, 4, 5, 6]);
-    let result = SLL.getNthFromLast(list, 0)
-    expect(result.value).toEqual(6);
+  // test('SLL.getNthFromLast(list, n) should return nth from last node', () => {
+  //   let list = SLL.fromArray([3, 4, 5, 6]);
+  //   let result = SLL.getNthFromLast(list, 0)
+  //   expect(result.value).toEqual(6);
 
-    result = SLL.getNthFromLast(list, 1);
-    expect(result.value).toEqual(5);
+  //   result = SLL.getNthFromLast(list, 1);
+  //   expect(result.value).toEqual(5);
 
-    result = SLL.getNthFromLast(list, 2);
-    expect(result.value).toEqual(4);
+  //   result = SLL.getNthFromLast(list, 2);
+  //   expect(result.value).toEqual(4);
 
-    result = SLL.getNthFromLast(list, 3);
-    expect(result.value).toEqual(3);
-  });
+  //   result = SLL.getNthFromLast(list, 3);
+  //   expect(result.value).toEqual(3);
+  // });
 
-  test('list.reduce(cb) should reduce list', () => {
-    let list = SLL.fromArray([1,2,3]);
-    let result = list.reduce((p, n) => p + n.value, 0);
-    expect(result).toEqual(6);
-  });
+  // test('list.reduce(cb) should reduce list', () => {
+  //   let list = SLL.fromArray([1,2,3]);
+  //   let result = list.reduce((p, n) => p + n.value, 0);
+  //   expect(result).toEqual(6);
+  // });
 
-  test('list.map(doubler) should double a list', () => {
-    let list = SLL.fromArray([1,2,3]);
-    let result = list.map(n => n.value * 2);
-    expect(result).toEqual(SLL.fromArray([2,4,6]));
-  });
+  // test('list.map(doubler) should double a list', () => {
+  //   let list = SLL.fromArray([1,2,3]);
+  //   let result = list.map(n => n.value * 2);
+  //   expect(result).toEqual(SLL.fromArray([2,4,6]));
+  // });
 
-  test('list.filter(isEven) should filter out odd numbers', () => {
-    let list = SLL.fromArray([1,2,3,4]);
-    let result = list.filter(n => n.value % 2 === 0);
-    expect(result).toEqual(SLL.fromArray([2,4]));
-  })
+  // test('list.filter(isEven) should filter out odd numbers', () => {
+  //   let list = SLL.fromArray([1,2,3,4]);
+  //   let result = list.filter(n => n.value % 2 === 0);
+  //   expect(result).toEqual(SLL.fromArray([2,4]));
+  // })
 
-  test('list.find(value) return fist node containg value', () => {
-    let list = SLL.fromArray([1,2,3,2,1]);
-    let result = list.find(2);
-    expect(list.next).toEqual(result);
-    result = result.next.find(2);
-    expect(result).toEqual(list.getNthFromLast(1));
-  });
+  // test('list.find(value) return fist node containg value', () => {
+  //   let list = SLL.fromArray([1,2,3,2,1]);
+  //   let result = list.find(2);
+  //   expect(list.next).toEqual(result);
+  //   result = result.next.find(2);
+  //   expect(result).toEqual(list.getNthFromLast(1));
+  // });
 
-  test('getNth shuold return the corect node', () => {
-    let list = SLL.fromArray([1,2,3,4]);
-    expect(list.getNth(0)).toEqual(list);
-    expect(list.getNth(1)).toEqual(list.next);
-    expect(list.getNth(2)).toEqual(list.next.next);
-    expect(list.getNth(3)).toEqual(list.next.next.next);
-  });
+  // test('getNth shuold return the corect node', () => {
+  //   let list = SLL.fromArray([1,2,3,4]);
+  //   expect(list.getNth(0)).toEqual(list);
+  //   expect(list.getNth(1)).toEqual(list.next);
+  //   expect(list.getNth(2)).toEqual(list.next.next);
+  //   expect(list.getNth(3)).toEqual(list.next.next.next);
+  // });
 
-  test('getFirst getSecond and getThird', () => {
-    let list = SLL.fromArray([1,2,3,4]);
-    expect(list.getFirst()).toEqual(list);
-    expect(list.getSecond()).toEqual(list.next);
-    expect(list.getThird()).toEqual(list.next.next);
-  });
-
-
+  // test('getFirst getSecond and getThird', () => {
+  //   let list = SLL.fromArray([1,2,3,4]);
+  //   expect(list.getFirst()).toEqual(list);
+  //   expect(list.getSecond()).toEqual(list.next);
+  //   expect(list.getThird()).toEqual(list.next.next);
+  // });
 })
--- curriculum/10-linked-lists/singly-linked-list/index.js
+++ curriculum/10-linked-lists/singly-linked-list/index.js
@@ -1,140 +1,198 @@
 'use strict';
 
-let SLL = module.exports = function(value, next=null){
-  this.value = value;
-  this.next = next;
-}
-
-SLL.append = (list, node) => {
-  let _append = (list, node) => {
-    if(!list.next)
-      return list.next = node;
-    _append(list.next, node);
-  };
-  _append(list, node);
-  return list;
-};
-
-SLL.prepend = (list, node) => {
-  node.next = list;
-  return node;
-};
-
-SLL.remove = (list, node) => {
-  if(list === node)
-    return node.next;
-
-  let _remove = (list, node) => {
-    if(list.next == node)
-      return list.next = list.next.next;
-    _remove(list.next, node);
-  };
-
-  _remove(list, node);
-  return list;
-};
-
-SLL.reduce = (list, cb, result) => {
-  let i=0;
-  let current = list
-  while(current){
-    result = cb(result, current, i++, list);
-    current = current.next;
+class ListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next || null;
   }
-  return result;
 }
 
-SLL.map = (list, cb) => {
-  let result = SLL.reduce(list, (prev, curr, i , self) => {
-    prev.append(new SLL(cb(curr, i, self)))
-    return prev;
-  }, new SLL())
-  return result.next;
-}
+class LinkedList {
+  constructor() {
+    this.root = null
+  }
 
-SLL.filter = (list, cb) => {
-  let result = SLL.reduce(list, (prev, curr, i, self) => {
-    if(cb(curr, i, self)) {
-      prev.append(new SLL(curr.value));
+  append(value) {
+    if (!this.root) {
+      this.root = new ListNode(value);
+      return
     }
-    return prev;
-  }, new SLL())
-  return result.next;
-}
 
-SLL.find = (list, value) => {
-  let current = list;
-  while(current){
-    if(current.value === value)
-      return current;
-    current = current.next;
+    // start with a pointer at the root
+    let current = this.root;
+
+    // step forward node-by-node until one has
+    // an empty next value
+    while (current.next) {
+      current = current.next;
+    }
+
+    // add the new value to the empty .next spot
+    // that's at the end of the list
+    current.next = new ListNode(value);
+  }
+
+  prepend(value) {
+    // make a new node with the value
+    // have the new node point to the current root.
+    // replace the root with the new node.
+    let node = new ListNode(value);
+    node.next = this.root;
+    this.root = node;
+  }
+
+  remove(value) {
+    // rewrite the root node if the value is at the front.
+    if(this.root.value === value) {
+      let result = this.root;
+      this.root = this.root.next;
+      return result;
+    }
+
+    // start at the front
+    // look for a node that points to the node we want to remove.
+    let current = this.root;
+    while (current.next.value !== value) {
+      current = current.next;
+    }
+
+    // save the result
+    // make the current node point to the node after the node we're removing
+    let result = current.next;
+    current.next = current.next.next;
+    return result;
+  }
+
+  reduce(list, cb, result) {
+    let i=0;
+    let current = list
+    while(current){
+      result = cb(result, current, i++, list);
+      current = current.next;
+    }
+    return result;
+  }
+
+  map(list, cb) {
+    let result = SLL.reduce(list, (prev, curr, i , self) => {
+      prev.append(new SLL(cb(curr, i, self)))
+      return prev;
+    }, new SLL())
+    return result.next;
+  }
+
+  filter(list, cb) {
+    let result = SLL.reduce(list, (prev, curr, i, self) => {
+      if(cb(curr, i, self)) {
+        prev.append(new SLL(curr.value));
+      }
+      return prev;
+    }, new SLL())
+    return result.next;
+  }
+
+  find(value) {
+    let current = this.root;
+    while(current){
+      if(current.value === value)
+        return current;
+      current = current.next;
+    }
+  }
+
+  reverse() {
+    let rest = this.root.next;
+    let result = this.root;
+    result.next = null;
+
+    let _reverse = (node) => {
+      let rest = node.next
+      let temp = node 
+      temp.next = null
+      result = SLL.prepend(result, temp)
+      if(rest) _reverse(rest)
+    }
+
+    _reverse(rest);
+    return result;
+  }
+
+  getMiddle(list) {
+    let slow = this.root;
+    let fast = this.root;
+    while(fast && fast.next){
+      slow = slow.next;
+      fast = fast.next.next
+    }
+    return slow
+  }
+
+  getNthFromLast(n) {
+    var i;
+    let result = list;
+    let offset = list;
+    for(i=0; i<n && offset.next; i++) {
+      offset = offset.next 
+    }
+    while(offset.next){
+      result = result.next;
+      offset = offset.next
+    }
+    return result
+  }
+
+  getLast() {
+    return this.getNthFromLast(0);
+  }
+  getSecondFromLast() {
+    return this.getNthFromLast(1);
+  }
+  getThirdFromLast() {
+    return this.getNthFromLast(2);
+  }
+
+  getNth(n) {
+    // step forward N times.
+    let current = this.root;
+    for(let i = 0; i < n; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  getFirst() {
+    return this.getNth(0);
+  }
+
+  getSecond() {
+    return this.getNth(1);
+  }
+
+  getThird() {
+    return this.getNth(2);
+  }
+
+  fromArray(items) {
+    // Build the list up backwards.
+    // Start by creating the last node that points to nothing.
+    // Then make the second-to-last node and point it
+    // to the last node.
+    // Then make another node and point it to the second-to-last node.
+    // Do this until the list is entirely built up.
+    let previousNode = null
+    for (var i = items.length - 1; i >= 0; i--) {
+      let value = items[i];
+      let node = new ListNode(value);
+      node.next = previousNode;
+      previousNode = node;
+    }
+
+    // set the root to point to the last node added at the front of the chain.
+    this.root = previousNode;
+
+    // return a reference to this list.
+    return this;
   }
 }
 
-SLL.reverse = (list) => {
-  let rest = list.next;
-  let result = list;
-  result.next = null;
-
-  let _reverse = (list) => {
-    let rest = list.next
-    let temp = list 
-    temp.next = null
-    result = SLL.prepend(result, temp)
-    if(rest) _reverse(rest)
-  }
-
-  _reverse(rest);
-  return result;
-}
-
-SLL.getMiddle = (list) => {
-  let slow = list;
-  let fast = list;
-  while(fast && fast.next){
-    slow = slow.next;
-    fast = fast.next.next
-  }
-  return slow
-}
-
-let getNthFromLast = n => list => {
-  var i;
-  let result = list;
-  let offset = list;
-  for(i=0; i<n && offset.next; i++)
-    offset = offset.next 
-  while(offset.next){
-    result = result.next;
-    offset = offset.next
-  }
-  return result
-}
-
-SLL.getLast = getNthFromLast(0)
-SLL.getSecondFromLast = getNthFromLast(1)
-SLL.getThirdFromLast = getNthFromLast(2)
-SLL.getNthFromLast = (list, n) => getNthFromLast(n)(list)
-
-let getNth = n => list => {
-   for(var result=list, i=0; i<n && result; i++, result=result.next);
-   if(i===n && result) return result;
-}
-
-SLL.getFirst = getNth(0)
-SLL.getSecond = getNth(1)
-SLL.getThird = getNth(2)
-SLL.getNth = (list, n) => getNth(n)(list)
-
-// ALL ABOVE METHODS WILL ALSO BE prototype methods
-SLL.prototype = Object.keys(SLL).reduce((methods, key) => {
-  methods[key] = function(...args){ return SLL[key](this, ...args) };
-  return methods;
-}, {})
-
-// all below methods are factory methods on SLL
-
-SLL.fromArray = items => {
-  return items.reverse().reduce((p, n) => SLL.prepend(p, new SLL(n)), null)
-}
-
+module.exports = {LinkedList, ListNode}
