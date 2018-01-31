527638778d0dd3d91bae28a8f0f92a866e00ec3f
Scott Schmidt <sjschmidt44@gmail.com>
2018-01-30 16:11:54 -0800

    add lecture demo for class13

--- curriculum/11-express-and-mongo/demos/11-d21/lib/error-handler.js
+++ curriculum/11-express-and-mongo/demos/11-d21/lib/error-handler.js
@@ -9,7 +9,7 @@ module.exports = function(err, res) {
 
   switch(true) {
     case msg.includes('validation error'): return res.status(400).send(`${err.name}: ${err.message}`)
-    case msg.includes('path error'): return res.status(404).send(`${err.name}: ${err.message}`)
+    case msg.includes('enoent'): return res.status(404).send(`${err.name}: ${err.message}`)
     case msg.includes('server error'): return res.status(500).send(`${err.name}: ${err.message}`)
     default: return res.status(500).send(`${err.name}: ${err.message}`)
   }
---
+++ curriculum/13-object-relational-mapping/demos/13-d21/model/track.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const debug = require('debug')('http:model-track')
+
+const Track = mongoose.Schema({
+  "speechiness": {type: Number},
+  "key": {type: Number},
+  "time_signature": {type: Number},
+  "liveness": {type: Number},
+  "loudness": {type: Number},
+  "duration_ms": {type: Number},
+  "danceability": {type: Number},
+  "duration": {type: Number},
+  "valence": {type: Number},
+  "acousticness": {type: Number},
+  "spotify_id": {type: String},
+  "volume_number": {type: Number},
+  "energy": {type: Number},
+  "tempo": {type: Number},
+  "instrumentalness": {type: Number},
+  "mode": {type: Number},
+  "number": {type: Number},
+  "artist": {type: String, required: true},
+  "title": {type: String, required: true},
+}, { timestamps: true })
+
+module.exports = mongoose.model('track', Track)
