515ee0e1ace803bfd14dd40307227a7f989247d7
Scott Schmidt <sjschmidt44@gmail.com>
2018-02-07 06:40:29 -0800

    update demo for class18

--- curriculum/18-asset-management/demos/18-d21/lib/aws-s3.js
+++ curriculum/18-asset-management/demos/18-d21/lib/aws-s3.js
@@ -2,20 +2,18 @@
 
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
-AWS.config.setPromisesDependency(Promise)
 
 const uploads = module.exports = {}
 
-// Can this be refactored to not use an explicit promise return?
 uploads.uploadProm = function(params) {
   return new Promise((resolve, reject) => {
     s3.upload(params, (err, data) => err ? reject(err) : resolve(data))
   })
 }
 
-uploads.deleteProm = function(params) {
-  return new Promise((resolve, reject) => {
-    s3.deleteObject(params, (err, data) =>
-      err ? reject(err.stack) : resolve(data))
-  })
-}
+// uploads.deleteProm = function(params) {
+//   return new Promise((resolve, reject) => {
+//     s3.deleteObject(params, (err, data) =>
+//       err ? reject(err.stack) : resolve(data))
+//   })
+// }
