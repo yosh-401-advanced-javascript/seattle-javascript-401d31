3bccf891ba68c55d4d87b5ee4b19b7204b2cd6dd
Scott Schmidt <sjschmidt44@gmail.com>
2018-02-23 14:16:19 -0800

    add lecture demo for day 31

--- curriculum/31-redux/demos/31-d21/lecture/webpack.config.js
+++ curriculum/31-redux/demos/31-d21/lecture/webpack.config.js
@@ -1,49 +1,49 @@
-'use strict';
+'use strict'
 
-require('dotenv').config({ path: `${__dirname}/.dev.env` });
-const production = process.env.NODE_ENV === 'production';
+require('dotenv').config({path: `${__dirname}/.dev.env`})
+const production = process.env.NODE_ENV === 'production'
 
-const { DefinePlugin, EnvironmentPlugin } = require('webpack');
-const HtmlPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const UglifyPlugin = require('uglifyjs-webpack-plugin');
-const ExtractPlugin = require('extract-text-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const { DefinePlugin, EnvironmentPlugin } = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 let plugins = [
-  new EnvironmentPlugin(['NODE_ENV']),
-  new ExtractPlugin('bundle-[hash].css'),
   new HtmlPlugin({ template: `${__dirname}/src/index.html` }),
+  new ExtractTextPlugin('bundle-[hash].css'),
+  new EnvironmentPlugin(['NODE_ENV']),
   new DefinePlugin({
-    __DEBUG__: JSON.stringify(!production)
-  })
+    __DEBUG__: JSON.stringify(!production),
+  }),
 ]
 
 if(production) {
-  plugins = plugins.concat([new CleanPlugin(), new UglifyPlugin()]);
+  plugins = plugins.concat([new CleanPlugin(), new UglifyPlugin()])
 }
 
 module.exports = {
   plugins,
-  entry: `${__dirname}/src/main.js`,
-  devServer: {
-    historyApiFallback: true
-  },
   devtool: production ? undefined : 'source-maps',
+  devServer: {
+    historyApiFallback: true,
+  },
+  entry: `${__dirname}/src/main.js`,
   output: {
     path: `${__dirname}/build`,
     filename: 'bundle-[hash].js',
-    publicPath: process.env.CDN_URL
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        loader: ExtractPlugin.extract(['css-loader', 'sass-loader'])
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
       },
       {
         test: /\.(woff|woff2|ttf|eot|glyph|\.svg)$/,
