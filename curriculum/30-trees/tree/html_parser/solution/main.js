13264b00c4ca976890a09dfa60ef249f41868f4e
John Cokos <highlander.44@gmail.com>
2018-05-14 16:38:45 -0700

    Move CI/FP to days 1 and 2; Insert and reorder all DSAs

--- curriculum/30-trees/tree/html_parser/solution/main.js
+++
@@ -1,8 +0,0 @@
-const parser = require('./parser');
-
-// there's tiny.html, minimal.html and stretch.html
-let filePath = '../tiny.html';
-if (process.argv.length > 2) {
-  filePath = process.argv[2];
-}
-parser(filePath);
