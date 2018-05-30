05abc9bbd7e2542fd6cd6b6dac9c5068ddcb225c
John Cokos <highlander.44@gmail.com>
2018-05-30 12:51:10 -0700

    Bugfix in the model finder -- returning the full model now, not the name

--- curriculum/12-express-middleware/demo/api-server/src/middleware/models.js
+++ curriculum/12-express-middleware/demo/api-server/src/middleware/models.js
@@ -22,8 +22,8 @@ const models = requireAll('../models');
 export default (req,res,next) => {
   try {
     let model = req && req.params && req.params.model;
-    if ( model && models[model] ) {
-      req.model = model;
+    if ( model && models[model] && models[model].default ) {
+      req.model = models[model].default;
       next();
     }
     else { throw 'Model not found'; }
