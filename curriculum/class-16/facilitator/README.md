672f3afa8eaec6b1af1b2f6b693dc25d8f17d6ba
John Cokos <highlander.44@gmail.com>
2019-04-05 21:49:58 -0700

    Class 16 Mods: Demo Notes

--- curriculum/class-16/facilitator/README.md
+++ curriculum/class-16/facilitator/README.md
@@ -33,18 +33,6 @@ It's important to start the students off with confirmation that they already kno
       * "Cool, thanks for letting me know, I don't care"
       * "Cool, thanks for letting me know, I need to do some stuff now"
 
-## Live Coding Demo(s)
-
-**`events`** - is a multi-file demo that shows how Node events work over multiple files/modules and how you must share a common core to make it all tie in.
-
-* Start in one file, by simply pulling in the events module and using `emit()` and `on()` to demonstrate simple Pub/Sub
-* Move to separate files 
-  * One only publishing, one only receiving
-  * Demonstrate that you cannot simply "require events" and go
-  * You'll need to create a common event controller for them to consume
-  * Once you to, each module can assume both roles
-  * And many more modules you add to the system can join the conversation
-
 ## Lab Prep
 * Students will do a full wiring of an event system.
 * They will be given a simple one-file system (like what you start with in the lecture) and tasked with:
--- curriculum/class-16/solution/code/app.js
+++ curriculum/class-16/solution/code/app.js
@@ -1,10 +1,32 @@
 'use strict';
 
+const fs = require('fs');
+const util = require('util');
+
+// Our event pool/monitor
 const events = require('./lib/events.js');
-const files = require('./lib/files.js');
+
+// Listen for things to happen and log them out
 const logger = require('./lib/logger.js');
 
+// -------------------------------------------------
+// Read in a file, save it, throw up some events...
+// -------------------------------------------------
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 let file = process.argv.slice(2).shift();
-if ( file ) {
-  files.alterFile(file);
-}
+
+const loadFile = (file) => readFile(file);
+
+const saveFile = (file,buffer) => writeFile(file,buffer);
+
+const convertBuffer = buffer => Buffer.from(buffer.toString().trim().toUpperCase());
+
+loadFile(file)
+  .then( contents => convertBuffer(contents) )
+  .then( buffer => saveFile(file,buffer) )
+  .then( () => events.emit('file-save', file) )
+  .catch( error => events.emit('file-error', error) );
+
