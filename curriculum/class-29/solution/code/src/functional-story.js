922a11e72f3441c8a64c904c600dd8bfbb9449cc
John Cokos <highlander.44@gmail.com>
2019-04-20 20:11:18 -0700

    Class 29 Retooling

---
+++ curriculum/class-29/solution/code/src/functional-story.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FunctionalDetails from './functional-details.js';
+
+export default () => (
+  <FunctionalDetails>
+    <span>1</span>
+    <span>2</span>
+    <span>3</span>
+  </FunctionalDetails>
+);
